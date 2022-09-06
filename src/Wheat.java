33eb7c88ce82051f6b9da14052131407c3153a56
Chaseyy <chaseanthony05@hotmail.com>
2022-09-06 14:10:59 +1000

    Added Wheat class

--- src/Wheat.java
+++ src/Wheat.java
@@ -1,10 +1,20 @@
 import java.awt.*;
 
 public class Wheat extends Plant{
+    private static int xPos, yPos;
 
-    public Wheat(Graphics g, int xPos, int yPos, int Width, int Height) {
+    public Wheat(Graphics g, int _xPos, int _yPos, int Width, int Height) {
+        xPos = _xPos;
+        yPos = _yPos;
+
         g.setColor(new Color(255,186,1));
-        g.fillRect(xPos, yPos, Width, Height);
+        g.fillRect(_xPos, _yPos, Width, Height);
+
+
+    }
+
+    public static int getXPosition() {
+        return xPos;
     }
 
 }
